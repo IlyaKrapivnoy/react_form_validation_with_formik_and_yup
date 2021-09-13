a9cfc3e19dce2d40a65a6d5140c624dcdf7c6ff5
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-13 14:07:48 +0300

    add iform field initial values

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,24 @@
+import { Formik, Form } from 'formik';
+
 const Signup = () => {
     return (
-        <div>
-            <h1>Sign Up</h1>
-        </div>
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+            }}
+        >
+            {(formik) => (
+                <div>
+                    <h1 className='my-4 font-weight-bold-display-4'>Sign Up</h1>
+                    {console.log(formik)}
+                    <Form></Form>
+                </div>
+            )}
+        </Formik>
     );
 };
 

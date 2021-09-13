a42e95dc0b1f16d283afc193996d77a9c901110c
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-13 13:55:53 +0300

    add sign up component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import rocketImg from './assets/rocket.png';
+import Signup from './components/Signup';
 
 function App() {
     return (
         <div className='container mt-3'>
             <div className='row'>
-                <div className='col-md-5'>Signup</div>
+                <div className='col-md-5'>
+                    <Signup />
+                </div>
                 <div className='col-md-7'>
                     <img
                         className='img-fluid w-100'

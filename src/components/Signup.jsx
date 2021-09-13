716f01398c85ca6771db35f9d36f359cdd6308a9
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-13 14:20:41 +0300

    add fields

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,5 @@
 import { Formik, Form } from 'formik';
+import TextField from './TextField';
 
 const Signup = () => {
     return (
@@ -15,7 +16,33 @@ const Signup = () => {
                 <div>
                     <h1 className='my-4 font-weight-bold-display-4'>Sign Up</h1>
                     {console.log(formik)}
-                    <Form></Form>
+                    <Form>
+                        <TextField
+                            label='First Name'
+                            value='firstName'
+                            type='text'
+                        />
+                        <TextField
+                            label='Last Name'
+                            value='firstName'
+                            type='text'
+                        />
+                        <TextField
+                            label='Email'
+                            value='firstName'
+                            type='text'
+                        />
+                        <TextField
+                            label='Password'
+                            value='firstName'
+                            type='text'
+                        />
+                        <TextField
+                            label='Confirm Password'
+                            value='firstName'
+                            type='text'
+                        />
+                    </Form>
                 </div>
             )}
         </Formik>
